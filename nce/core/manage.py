24e9c181edb88d72d5af81cb95f41462f8a6f838
ShellCode33 <shellcode33@protonmail.ch>
2019-02-18 20:45:32 +0100

    Sessions are now sorted correctly

--- nce/core/manage.py
+++ nce/core/manage.py
@@ -1,65 +1,57 @@
 # coding: utf-8
 
 from nce.core import logger
-from scapy.all import rdpcap
+from scapy.all import *
 
 
-def session_extractor(p):
+def session_extractor(pkt):
     """Extract sessions from packets. Has been taken from here :
     https://github.com/secdev/scapy/blob/master/scapy/plist.py#L505
 
     By default A talking to B and B answering to A are 2 different sessions. We want them to be the same.
-    We simply apply an alphabetic sort on IP:PORT
+    We simply apply an alphabetic sort on IP:PORT to determine which one will go first in the string.
     """
 
-    if 'Ether' in p:
-        if 'IP' in p or 'IPv6' in p:
-            ip_src_fmt = "{IP:%IP.src%}{IPv6:%IPv6.src%}"
-            ip_dst_fmt = "{IP:%IP.dst%}{IPv6:%IPv6.dst%}"
+    if "IP" in pkt:
+        pkt_type = "IP"
+    elif "IPv6" in pkt:
+        pkt_type = "IPv6"
+    else:
+        return "WeDontCare"
 
-            addr_fmt = (ip_src_fmt, ip_dst_fmt)
+    src = "{}:{}".format(pkt[pkt_type].src, pkt[pkt_type].sport)
+    dst = "{}:{}".format(pkt[pkt_type].dst, pkt[pkt_type].dport)
 
-            if 'TCP' in p:
-                fmt = "TCP {}:%r,TCP.sport% | {}:%r,TCP.dport%"
-            elif 'UDP' in p:
-                fmt = "UDP {}:%r,UDP.sport% | {}:%r,UDP.dport%"
-            elif 'ICMP' in p:
-                fmt = "ICMP {} | {} type=%r,ICMP.type% code=%r," \
-                      "ICMP.code% id=%ICMP.id%"
-            elif 'ICMPv6' in p:
-                fmt = "ICMPv6 {} | {} type=%r,ICMPv6.type% " \
-                      "code=%r,ICMPv6.code%"
-            elif 'IPv6' in p:
-                fmt = "IPv6 {} | {} nh=%IPv6.nh%"
-            else:
-                fmt = "IP {} | {} proto=%IP.proto%"
-            return p.sprintf(fmt.format(*addr_fmt))
-        elif 'ARP' in p:
-            return p.sprintf("ARP %ARP.psrc% | %ARP.pdst%")
-        else:
-            return p.sprintf("Ethernet type=%04xr,Ether.type%")
+    if src < dst:
+        return src + " | " + dst
+    else:
+        return dst + " | " + src
 
-    return "Other"
 
 def process_pcap(filename):
     logger.info("Processing packets in '{}'".format(filename))
     pcap = rdpcap(filename)
 
-    pcap = pcap.sessions()
+    pcap = pcap.sessions(session_extractor)
 
     print(pcap)
 
     i = 0
 
     for pkt in pcap:
-        #pkt.show()
+        # pkt.show()
         if hasattr(pkt, "load"):
             print(pkt.load)
-            print("-"*30)
+            print("-" * 30)
         i += 1
 
         if i == 45:
             break
 
+
 def active_processing(interface):
     logger.info("Listening on {}...".format(interface))
+
+
+if __name__ == "__main__":
+    process_pcap("tests/samples/telnet-cooked.pcap")
