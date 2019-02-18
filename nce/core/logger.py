ddeb513b354ad1a19c6faa469a5403973117d3fd
ShellCode33 <shellcode33@protonmail.ch>
2019-02-18 21:54:23 +0100

    Add colors to logger

--- nce/core/logger.py
+++ nce/core/logger.py
@@ -1,16 +1,20 @@
 # coding: utf-8
 
+
 class Color(object):
-    pass
+    BRIGHT_BLUE = "\u001b[34;1m"
+    RED = "\u001b[31m"
+    GREEN = "\u001b[32m"
+    RESET = "\u001b[0m"
 
 
 def debug(msg):
-    print("[DEBUG] {}".format(msg))
+    print("{}[DEBUG]{} {}".format(Color.BRIGHT_BLUE, Color.RESET, msg))
 
 
 def info(msg):
-    print("[INFO] {}".format(msg))
+    print("{}[INFO]{}  {}".format(Color.GREEN, Color.RESET, msg))
 
 
 def error(msg):
-    print("[ERROR] {}".format(msg))
+    print("{}[ERROR]{} {}".format(Color.RED, Color.RESET, msg))
