d74670e3810a0ca2129b90eebba025d3579e06ae
ShellCode33 <shellcode33@protonmail.ch>
2019-03-05 00:12:02 +0100

    Fix bug

--- nce/parsers/telnet.py
+++ nce/parsers/telnet.py
@@ -40,6 +40,7 @@ def analyse(packets: PacketList) -> CredentialsList:
     # We don't stop the loop even if we find a username/password because
     # if we find others it means a wrong password has been entered
     for string in strings:
+
         potential_username_tokens = string.split(" ")
 
         # -1 is the username, -2 the "asking" part
@@ -49,7 +50,7 @@ def analyse(packets: PacketList) -> CredentialsList:
             if _is_username_duplicated(username):
                 username = "".join([username[i] for i in range(0, len(username), 2)])
 
-        elif "password:" in string.lower():
+        elif string.lower().startswith("password:"):
             begin_pass_index = string.find(":") + 1
 
             # Prone to false positives, but sometimes the telnet server sends "password:" and sometimes "password: "
